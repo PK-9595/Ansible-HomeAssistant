beb7beea905f39fbe1629522468e45b7406ccfa2
Paul Chien - Aftershock Laptop <paulchien_5@hotmail.com>
2024-11-13 11:12:40 +0800

    - Added .ansiblelog files to .gitignore (in case I want to log things) - Added retries, delays, and desired outcome for ansible task to download docker compose binary - Changed location to check if HACS is already installed - Removed restriction of setting mariadb container's user (leave as default - root) - Added (but commented out) logging of ansible playbooks

--- load_env.sh
+++ load_env.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 export $(grep -v '^#' .env | xargs)
+DEBUG_LOG="./$(date "+%Y-%m-%d_%H-%M-%S").ansiblelog"
+
 ansible-playbook -i Ansible/inventory Ansible/playbook.yml
+# ansible-playbook -i Ansible/inventory -vvv Ansible/playbook.yml > "${DEBUG_LOG}" | grep -E '^TASK|^PLAY|^ok|^changed|^failed|^skipping'
